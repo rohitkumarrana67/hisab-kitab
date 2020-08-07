c76556a364b7626e6ffb772d90f3d476a11a2b9c
Vaibhav Srivastava <vs37559@gmail.com>
2020-08-07 23:43:20 +0530

    Fixed errors.

--- v1/entity_builders/customer_builder.js
+++ v1/entity_builders/customer_builder.js
@@ -9,11 +9,20 @@ CustomerEntityBuilder.prototype.create = function (req_data) {
     return { user_id, customer_id, customer_name, email, mobile_number, address };
 }
 
-CustomerEntityBuilder.prototype.getCustomers = function (req_data) {
+CustomerEntityBuilder.prototype.getCustomers = async function (req_data) {
     res_data = [];
-    req_data.forEach(data => {
-        const { user_id, customer_id, customer_name, email, mobile_number, address, balance } = {...data, ...utility.getBalance(data.user_id, data.customer_id)};
-        res_data.push({ user_id, customer_id, customer_name, email, mobile_number, address, balance });
-    });
+    for(data of req_data){
+        const balance = await utility.getBalance(data.user_id, data.customer_id)
+        const entity_obj = {
+            user_id :data.user_id,
+            customer_id : data.customer_id,
+            customer_name : data.customer_name,
+            email : data.email,
+            mobile_number : data.mobile_number,
+            address : data.address,
+            balance : balance
+        }
+        res_data.push(entity_obj);
+    };
     return res_data;
 }
