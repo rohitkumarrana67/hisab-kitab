7be7e899b3f547f6c25a13b817d5f1fb6317de42
ROHIT KUMAR RANA <48640973+rohitkumarrana67@users.noreply.github.com>
2020-08-11 15:50:11 +0530

    added transaction editable modal

--- public/js/views/summary_view.js
+++ public/js/views/summary_view.js
@@ -20,15 +20,14 @@ var SummaryView = Backbone.View.extend({
     },
     render: function () {
         this.$el.html(this.template({ model: this.model }))
-        var transactions = new TransactionModel();
         var customer_id = this.model.get('customer_id');
         var $transactions_list = $("#transactions");
         this.collection.fetch({
             url: "http://localhost:3060/users/customer/" + customer_id + "/transactions",
             headers: { 'auth-token': localStorage.getItem('khata-token') },
             success: function (response) {
-                response.each(transaction => {
-                    $transactions_list.append((new TransactionView({ model: transaction })).render().$el);
+                response.each(data => {
+                    $transactions_list.append((new TransactionView({ model: data })).render().$el);
                 });
             },
             error: function (err, response) {
