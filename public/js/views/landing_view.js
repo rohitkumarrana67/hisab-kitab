ef632ac082303af97ac9a823a917c4684f9e3b35
ROHIT KUMAR RANA <48640973+rohitkumarrana67@users.noreply.github.com>
2020-08-13 23:30:55 +0530

    fixed bug

--- public/js/views/landing_view.js
+++ public/js/views/landing_view.js
@@ -13,15 +13,16 @@ var LandingView = Backbone.View.extend({
         this.template = _.template($('#home-template').html())
         this.render()
     },
-    render: function(){
-        this.$el.parent().parent().find('#content').html(this.template())
+    render: function () {
         if (!localStorage.getItem('khata-token')) {
             $('.before-auth').show()
             $('.after-auth').hide()
         } else {
             $('.before-auth').hide()
             $('.after-auth').show()
+            window.location = "#customers";
         }
+        this.$el.parent().parent().find('#content').html(this.template())
     }
 
 })
