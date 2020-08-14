4a92845ac508b42e2046baf519fb02cfeec7ab00
ROHIT KUMAR RANA <48640973+rohitkumarrana67@users.noreply.github.com>
2020-08-14 11:51:53 +0530

    added update password route

--- v1/entity_builders/user_builder.js
+++ v1/entity_builders/user_builder.js
@@ -17,7 +17,13 @@ UserEntityBuilder.prototype.login = function (req_data) {
 
 
 UserEntityBuilder.prototype.userProfile = function (req_data) {
-    // console.log(req_data)
-    const { name, email, address, mobile_number} = req_data;
+    const { name, email, address, mobile_number } = req_data;
     return { name, email, address, mobile_number };
+}
+
+UserEntityBuilder.prototype.updatePassword = function (req_data) {
+    return {
+        success: true,
+        message: "password successfully updated"
+    }
 }
--- v1/services/user_service.js
+++ v1/services/user_service.js
@@ -2,7 +2,7 @@ const UserDispatcher = require("../data_dispatchers/user_dispatcher");
 const UserEntityBuilder = require("../entity_builders/user_builder");
 
 module.exports = UserService = function (req_data, user_info) {
-    this.dispatcher = new UserDispatcher(req_data,user_info);
+    this.dispatcher = new UserDispatcher(req_data, user_info);
     this.builder = new UserEntityBuilder(req_data);
 }
 
@@ -19,4 +19,9 @@ UserService.prototype.login = async function (req_data) {
 UserService.prototype.update = async function () {
     const dispatcher_data = await this.dispatcher.update()
     return this.builder.userProfile(dispatcher_data)
+}
+
+UserService.prototype.updatePassword = async function () {
+    const dispatcher_data = await this.dispatcher.updatePassword();
+    return this.builder.updatePassword(dispatcher_data)
 }
